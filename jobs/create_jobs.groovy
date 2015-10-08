fc42b3720cb9b978b9b8f9a56c88745d7fca094a
Mike Paßberg <mike.passberg@asideas.de>
2015-10-08 17:02:30 +0200

    Inititial commit

---
+++ jobs/create_jobs.groovy
@@ -0,0 +1,23 @@
+//@GrabConfig(systemClassLoader=true)
+//@Grab(group='org.yaml', module='snakeyaml', version='1.5')
+
+import org.yaml.snakeyaml.Yaml
+
+def yaml = new Yaml()
+def object = yaml.load(new File('resources/test_data.yml').text)
+assert object instanceof Map
+
+object.applications.each {
+    String applicationame, Map config -> createJobs(applicationame, config)
+}
+
+def createJobs(String applicationname, Map config){
+    def environments = ['staging', 'prod']
+    environments.each {
+        String environment -> createJob(environment, applicationname, config)
+    }
+}
+
+def createJob(String environment, String applicationname, Map config){
+    println("$applicationname-$environment: $config")
+}
