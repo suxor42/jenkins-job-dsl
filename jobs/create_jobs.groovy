9bfd5d1a2dc42c533994898ce172409299781456
Mike Paßberg <mike.passberg@asideas.de>
2015-10-09 13:59:21 +0200

    Inititial commit

--- jobs/create_jobs.groovy
+++ jobs/create_jobs.groovy
@@ -1,6 +1,7 @@
 //@GrabConfig(systemClassLoader=true)
 //@Grab(group='org.yaml', module='snakeyaml', version='1.5')
 
+
 import org.yaml.snakeyaml.Yaml
 
 def file
@@ -15,17 +16,33 @@ println(file.canonicalPath)
 def object = yaml.load(file.text)
 assert object instanceof Map
 
+
+
 object.applications.each {
-    String applicationame, Map config -> createJobs(applicationame, config)
+    String applicationame, Map config ->
+        createJobs(applicationame, config, object.environments.keySet() as List)
 }
 
-def createJobs(String applicationname, Map config){
-    def environments = ['staging', 'prod']
+def createJobs(String applicationname, Map config, List environments){
     environments.each {
-        String environment -> createJob(environment, applicationname, config)
+        environment -> createJob(environment, applicationname, config)
     }
 }
 
 def createJob(String environment, String applicationname, Map config){
     println("$applicationname-$environment: $config")
+
+    job("$applicationname-$environment") {
+        scm {
+            git("https://github.com/as-ideas/yana-$applicationname")
+        }
+        triggers {
+            scm('*/1 * * * *')
+        }
+        steps {
+            shell("eb deploy $environment")
+        }
+    }
+
+
 }
